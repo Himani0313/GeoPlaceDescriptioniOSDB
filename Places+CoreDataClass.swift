bd184ec2f42a62b5c877bd86f106c6b93286526f
Himani Shah <hjshah2@asu.edu>
2017-04-22 19:44:58 -0700

    nsmanaged object model

---
+++ Places+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Places+CoreDataClass.swift
+//  GeoPlaceDescription
+//
+//  Created by hjshah2 on 4/22/17.
+//  Copyright © 2017 hjshah2. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class Places: NSManagedObject {
+
+}
